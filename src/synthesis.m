28298c0daf9b5d33ddd880d4c267051fc600b626
Bernhard <ga78col@linux75.clients.eikon.tum.de>
2018-08-09 16:44:35 +0200

    add synthesis try..

--- src/synthesis.m
+++ src/synthesis.m
@@ -1,6 +1,17 @@
-function [Im3] = synthesis(disparity, p)
+function [Im3] = synthesis(Im1, disparity, p)
 % Based on Disparitymap and rate of shift p
 % create a new Image Im3 with a camera on position O3
-Im3 = disparity * p;
+imshow(imread('disp0-n.pgm'));
+imshow(imread('disp1-n.pgm'));
+Im3 = zeros(size(Im1));
+
+for x=1:size(disparity, 1)
+    for y =1:size(disparity, 2)
+        % map pixel from Im1 to Im3
+        pos = x + disparity(x, y) * p;
+        Im3(pos, y) = Im1(x, y);
+    end
+end
+imshow(Im3);
 end
 
