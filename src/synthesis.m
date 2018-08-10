5fcacda68a71cc9494aebc845abdf66ff2d08281
Lukas Bernhard <L.J.Bernhard@web.de>
2018-08-10 15:17:29 +0200

    sceleton done, dont know if disparity images are good and if synthesis is working

--- src/synthesis.m
+++ src/synthesis.m
@@ -1,17 +1,18 @@
-function [Im3] = synthesis(Im1, disparity, p)
+function [Im3, Im4] = synthesis(ImL, dispL, ImR, dispR, p)
 % Based on Disparitymap and rate of shift p
 % create a new Image Im3 with a camera on position O3
-imshow(imread('disp0-n.pgm'));
-imshow(imread('disp1-n.pgm'));
-Im3 = zeros(size(Im1));
+Im3 = zeros(size(ImL));
+Im4 = zeros(size(ImR));
 
-for x=1:size(disparity, 1)
-    for y =1:size(disparity, 2)
-        % map pixel from Im1 to Im3
-        pos = x + disparity(x, y) * p;
-        Im3(pos, y) = Im1(x, y);
+for x=1:size(ImL, 1)
+    for y =1:size(ImL, 2)
+        % map pixel from ImL to Im3
+        pos = x + dispL(x, y) * p;
+        Im3(pos, y) = ImL(x, y);
+
+        pos = x + dispR(x, y) * p;
+        Im4(pos, y) = ImR(x, y);
     end
 end
-imshow(Im3);
 end
 
