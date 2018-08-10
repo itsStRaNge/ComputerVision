03cf4f32bb711c1d8cc1ffa708740408020bc213
Lukas Bernhard <L.J.Bernhard@web.de>
2018-08-10 10:55:12 +0200

    add test script for rectification

---
+++ src/RectifKitE/test_cv_rectify.m
@@ -0,0 +1,41 @@
+function test_cv_rectify()
+IL = imread('images/Sport0.png');
+IR = imread('images/Sport1.png');
+
+load('data/calibration');
+
+[JL, JR] = cv_rectify(IL, KL, IR, KR);
+
+
+% -------------------- PLOT LEFT VIEW
+%figure(1)
+subplot(2,2,1)
+image(IL);
+axis image
+title('Left image');
+
+% -------------------- PLOT RIGHT VIEW
+%figure(2)
+subplot(2,2,2)
+image(IR);
+axis image
+title('Right image');
+
+% -------------------- PLOT LEFT
+%figure(3)
+subplot(2,2,3)
+image(JL);
+axis image
+hold on
+title('Rectified left image');
+
+
+% --------------------  PLOT RIGHT
+%figure(4)
+subplot(2,2,4)
+image(JR);
+axis image
+hold on
+title('Rectified right image')
+
+end
