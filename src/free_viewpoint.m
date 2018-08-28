0a86ac94874ac0587d936888ef47b9c0102ac020
Bernhard <ga78col@linux86.clients.eikon.tum.de>
2018-08-28 17:28:18 +0200

    adjust console time output

--- src/free_viewpoint.m
+++ src/free_viewpoint.m
@@ -9,7 +9,7 @@ K = camera_param.IntrinsicMatrix';
 fprintf('Step\t Task\t\t\t\t Time Est.\tTime\n');
 
 %% undistortion lens from images
-fprintf('1/8\t Undistorting Lens\t\t 9.00s'); 
+fprintf('1/8\t Undistorting Lens\t\t 1.50s'); 
 start = tic;
 IL_d = undistort_image(IL,camera_param.FocalLength(1),camera_param.PrincipalPoint(1),...
                        camera_param.PrincipalPoint(2),...
@@ -28,7 +28,7 @@ IR_d = undistort_image(IR,camera_param.FocalLength(1),camera_param.PrincipalPoin
 fprintf('\t\t%.2fs\n', toc(start));
 
 %% feature matching
-fprintf('2/8\t Extracting Features\t\t 26.00s'); 
+fprintf('2/8\t Extracting Features\t\t 25.00s'); 
 start = tic;
 Corr = feature_extracting_matching(IL_d,IR_d,false);
 fprintf('\t\t%.2fs\n', toc(start));
@@ -40,13 +40,13 @@ E = eight_point_algorithm(Corr, K);
 fprintf('\t\t%.2fs\n', toc(start));
 
 %% compute eukledian motion
-fprintf('4/8\t Computing Motion\t\t 0.20s'); 
+fprintf('4/8\t Computing Motion\t\t 0.10s'); 
 start = tic;
 [R, T] = motion_estimation(Corr, E, K);
 fprintf('\t\t%.2fs\n', toc(start));
 
 %% rectificate images (crop or not)
-fprintf('5/8\t Apply Rectification\t\t 4.00s');
+fprintf('5/8\t Apply Rectification\t\t 3.80s');
 start = tic;
 [JL, JR, HomographyL, HomographyR] = rectification(IL, IR, R, T', K,'kit');
 fprintf('\t\t%.2fs\n', toc(start));
@@ -55,7 +55,7 @@ fprintf('\t\t%.2fs\n', toc(start));
 % TODO: evt zweite disparity map für rechtes bild berechnen
 % TODO: disparity map fct umschreiben, sodass korrespondenzpunkte
 % übergeweben werden können
-fprintf('6/8\t Creating Disparity Map\t\t 35.00s');
+fprintf('6/8\t Creating Disparity Map\t\t 34.00s');
 start = tic;
 [disparity_map,IL_resized,IR_resized] = calculateDisparityMap(JL,JR,'mode','block','size',800);
 fprintf('\t\t%.2fs\n', toc(start));
@@ -64,7 +64,7 @@ fprintf('\t\t%.2fs\n', toc(start));
 % NOTE: alle verabeitungsschritte vorher nur einmal auszuführen
 %       synthese wird bei jedem neuen p durchgeführt
 % TODO: holefilling nochmal anschaun
-fprintf('7/8\t Synthesising new Image\t\t 2.00s');
+fprintf('7/8\t Synthesising new Image\t\t 1.80s');
 start = tic;
 IM = synthesis(disparity_map, IL_resized, IR_resized, p);
 fprintf('\t\t%.2fs\n', toc(start));
